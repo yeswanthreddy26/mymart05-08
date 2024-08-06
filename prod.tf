453cf1b1a57ee841f649e971b550e6baef47a1ac
yeswanthreddy26 <156288135+yeswanthreddy26@users.noreply.github.com>
2024-08-06 12:54:05 +0530

    Update prod.tf

--- prod.tf
+++ prod.tf
@@ -19,24 +19,18 @@ provisioner "remote-exec" {
         "sudo apt install python3-pip -y",
         "sudo apt install python3-venv -y",
         "sudo apt install python3-virtualenv -y",
-        "python3 -m venv /home/ubuntu/praveen",
-        ". /home/ubuntu/praveen/bin/activate",
-        "git clone https://github.com/Praveenchinna14/teerdha19.git",
-        "cd teerdha19",
-        "sudo apt install libmysqlclient-dev -y",
-        "sudo apt install pkg-config -y",
-        "pip install -r requirements.txt",
-        "pip install django",
-        "pip install django-crsipy-forms",
-        "pip install django-rest-framework",
-        "pip install requests",
-        "pip install pyscopg2-binary",
-        "pip install wheel",
-        "pip install pillow",
-        "pip install easy-pil",
-        "python3 /home/ubuntu/teerdha19/manage.py makemgrations",
-        "python3 /home/ubuntu/teerdha19/manage.py migrate",
-        "python3 /home/ubuntu/teerdha19/manage.py runserver 0.0.0.0:8000"     
+        "python3 -m venv /home/ubuntu/reddy",
+        ". /home/ubuntu/reddy/bin/activate",
+        "git clone https://github.com/yeswanthreddy26/mymart05-08.git",
+        "cd mymaer05-08",
+        "sud0 apt install openjdk-17-jdk -y",
+        "sudo apt install maven -y",
+        "pip install spring -y",
+        "pip install gradle -y",
+        "mvn clean package",
+        "java -jar target/MyMart-0.0.1-SNAPSHOT.jar",
+        "nohup java -jar target/MyMart-0.0.1-SNAPSHOT.jar > spring_boot.log 2>&1 &"
+     
     ]
     connection {
       type     = "ssh"
