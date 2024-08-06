db98f2f5c7e4db6ad8181c18d758067fb6efa383
yeswanthreddy26 <156288135+yeswanthreddy26@users.noreply.github.com>
2024-08-06 18:13:18 +0530

    Delete src directory

--- src/main/java/com/mymart/controller/ProductCategoryController.java
+++
@@ -1,6 +0,0 @@
-package com.mymart.controller;
-
-
-public class ProductCategoryController {
-
-}
--- src/main/java/com/mymart/repository/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mymart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mymart.model.ProductCategory;
-
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
-
-}
