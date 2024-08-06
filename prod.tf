972eff9c92842658b8b5557fe291b27560770a91
yeswanthreddy26 <156288135+yeswanthreddy26@users.noreply.github.com>
2024-08-06 14:04:17 +0530

    Update prod.tf

--- prod.tf
+++ prod.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   alias = "us-east"
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "name_Prod" {
-  ami = "ami-0862be96e41dcbf74"
+  ami = "ami-04a81a99f5ec58529"
   instance_type = "t2.medium"
   key_name = "praveen"
-  vpc_security_group_ids = [ "sg-0e7783eb3e866cde2" ]
+  vpc_security_group_ids = [ "sg-0a5c5fa3d8e5c5b7e" ]
   tags = {
-    Name = "prof.tf"
+    Name = "prof.tfmymart"
   }
   
 provisioner "remote-exec" {
